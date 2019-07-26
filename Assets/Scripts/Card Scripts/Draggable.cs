91f1cbd1125d43f3be459e706107b6e2c7c90c38
Alex <alex.albrecht@hotmail.de>
2019-07-26 10:19:52 +0200

    Drag and Drop System

---
+++ Assets/Scripts/Card Scripts/Draggable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;  // 1
+
+//Code nur für Drag and Drop Funktion = 1
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler // 1
+{
+    public Transform parentToReturnTo = null;                                  // sets the parent (Hand)
+
+   public void OnBeginDrag(PointerEventData eventData) // 1
+    {
+        Debug.Log("OnBeginDrag"); // 1
+
+        parentToReturnTo = this.transform.parent;                       // Wenn die angewählte Karte aus der Hand geschoben wird,
+        this.transform.SetParent(this.transform.parent.parent);         // Ordnen sich die übrigen neu an.
+
+        GetComponent<CanvasGroup>().blocksRaycasts = false;             // Die Raycasts werden zum Zeiger durch die Karte (CanvasGroup) nicht mehr geblockt. 
+    }
+    public void OnDrag(PointerEventData eventData) // 1
+    {
+        //Debug.Log("OnDrag"); // 1
+        this.transform.position = eventData.position; // 1
+    }
+    public void OnEndDrag(PointerEventData eventData) // 1
+    {
+        Debug.Log("OnEndDrag"); // 1
+        this.transform.SetParent(parentToReturnTo);                     // Die Karte wird beim loslassen zurück in die Hand eingeordnet
+        GetComponent<CanvasGroup>().blocksRaycasts = true;              // Raycasts werden wieder durch Karte geblockt.
+    }
+}       //22:40
