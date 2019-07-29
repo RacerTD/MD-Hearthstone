ee1dd596f3e12a5d9768819c528ede6abef6cfa5
Alex <alex.albrecht@hotmail.de>
2019-07-29 11:43:34 +0200

    Drag and Drop Implementierung

--- Assets/Scripts/Card Scripts/Draggable.cs
+++ Assets/Scripts/Card Scripts/Draggable.cs
@@ -11,6 +11,7 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
    public void OnBeginDrag(PointerEventData eventData) // 1
     {
         Debug.Log("OnBeginDrag"); // 1
+        
 
         parentToReturnTo = this.transform.parent;                       // Wenn die angewählte Karte aus der Hand geschoben wird,
         this.transform.SetParent(this.transform.parent.parent);         // Ordnen sich die übrigen neu an.
@@ -25,6 +26,7 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
     public void OnEndDrag(PointerEventData eventData) // 1
     {
         Debug.Log("OnEndDrag"); // 1
+        this.transform.position = parentToReturnTo.transform.position;
         this.transform.SetParent(parentToReturnTo);                     // Die Karte wird beim loslassen zurück in die Hand eingeordnet
         GetComponent<CanvasGroup>().blocksRaycasts = true;              // Raycasts werden wieder durch Karte geblockt.
     }
