b5f3aeeb6d350b45b022b22ad18483f81892629d
Tobias <tobiasdeyle@googlemail.com>
2019-07-30 15:27:01 +0200

    Nur Jetzt auch mit Hintergrund

--- Assets/Scripts/Battlefield/CardsSideBySide.cs
+++ Assets/Scripts/Battlefield/CardsSideBySide.cs
@@ -8,6 +8,7 @@ public class CardsSideBySide : MonoBehaviour
     public float cardOffSetHorizontal;
     public float cardOffSetDepth;
     public float rotationOffSet;
+    public float scale;
     Transform myChild;
     int childCount;
     float startingAngle;
@@ -32,8 +33,9 @@ public class CardsSideBySide : MonoBehaviour
         for (int i = 0; i < transform.childCount; i++)
         {
             myChild = transform.GetChild(i);
-            myChild.transform.localPosition = new Vector3((i * cardOffSetHorizontal + startingPosition), (i * cardOffSetVertical), (i * cardOffSetDepth));
+            myChild.transform.localPosition = new Vector3((i * cardOffSetHorizontal + startingPosition), ( cardOffSetVertical), (i * cardOffSetDepth));
             myChild.transform.eulerAngles = new Vector3(0, 0, startingAngle + rotationOffSet * i);
+            myChild.transform.localScale = new Vector3(scale, scale, scale);
         }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,63 @@ public class GameManager : MonoBehaviour
     public GameObject hand;
     public GameObject enemyField;
     public bool playersTurn;
+    public enum GameState
+    {
+        Enemy,
+        PlayerCardDraw,
+        PlayerIdle,
+        PlayerCardInHand,
+        PlayerAttack,
+        PlayerAbility
+    }
+
+    public GameState gameState;
+
+    IEnumerator EnemyState()
+    {
+        while (gameState == GameState.Enemy)
+        {
+            yield return 0;
+        }
+    }
+    IEnumerator PlayerCardDrawState()
+    {
+        while (gameState == GameState.PlayerCardDraw)
+        {
+            yield return 0;
+        }
+    }
+    IEnumerator PlayerIdleState()
+    {
+        while (gameState == GameState.PlayerIdle)
+        {
+            yield return 0;
+        }
+    }
+    IEnumerator PlayerCardInHandState()
+    {
+        while (gameState == GameState.PlayerCardInHand)
+        {
+            yield return 0;
+        }
+    }
+    IEnumerator PlayerAttackState()
+    {
+        while (gameState == GameState.PlayerAttack)
+        {
+            yield return 0;
+        }
+    }
+    IEnumerator PlayerAbilityState()
+    {
+        while (gameState == GameState.PlayerAbility)
+        {
+            yield return 0;
+        }
+    }
+
+
+
 
     void Awake()
     {
