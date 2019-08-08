7ddcd98575ce0838e654d5c23877d665105ba3ac
Niiiiiiiiiiii <nici@bambusource.de>
2019-08-08 15:43:29 +0200

    updated Card.prefab added waves tooltip refinements

--- Assets/Scripts/Card Scripts/Tooltip.cs
+++ Assets/Scripts/Card Scripts/Tooltip.cs
@@ -1,19 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
+using DG.Tweening;
 
 public class Tooltip : MonoBehaviour
 {
     public List<GameObject> objects = new List<GameObject>();
     float waitTime = 0.01f;
-    float duration1 = 0.2f;
-    float duration2 = 0.4f;
-    float duration3 = 0.5f;
-    float duration4 = 0.6f;
+    float duration = 1f;
     float timer = 0.0f;
     float durationTimer=0.0f;
     bool t = false;
+    Transform myChild;
 
     // Start is called before the first frame update
     void Start()
@@ -25,64 +25,41 @@ public class Tooltip : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
-        if (Input.GetKeyDown("t") && t==(false))
+        if (Input.GetKeyDown("t") && t==false)
         {
             for (int i = 0; i <= objects.Count - 1; i++)
             {
                 objects[i].transform.position = new Vector3(-1.0f, 2.0f, 0.0f);
-                //objects[i].GetComponent<SpriteRenderer>().color = new Vector4(0f, 0f, 0f, 0f);
+
+                //myChild = transform.GetChild(0);
+                //myChild.GetComponent<Image>().color = new Vector4(0,0,0,0);
+
                 objects[i].SetActive(true);
             }
             t = true;
             durationTimer = 0.0f;
         }
         durationTimer += Time.deltaTime;
-        if (timer > waitTime && t && durationTimer < duration1)
+
+        if (timer > waitTime && t && durationTimer < duration)
         {
             for (int i = 0; i <= objects.Count - 1; i++)
             {
+                //objects[i].GetComponentsInChildren<Image>()
                 timer = timer - waitTime;
-                objects[i].transform.position = objects[i].transform.position + new Vector3(3f, 0.0f, 0.0f) * Time.deltaTime;
-                //objects[i].GetComponent<SpriteRenderer>().color = new Vector4(0f, 0f, 0f, 0.3f);
+                objects[i].transform.DOMove(new Vector3(1, 2, 0), 1).SetEase(Ease.OutQuart);
+                //myChild.GetComponent<Image>().color = new Vector4(0, 0, 0, 1) * (durationTimer / duration);
             }
         }
 
-        if (timer > waitTime && t && durationTimer < duration2)
-        {
-            for (int i = 0; i <= objects.Count - 1; i++)
-            {
-                timer = timer - waitTime;
-                objects[i].transform.position = objects[i].transform.position + new Vector3(2f, 0.0f, 0.0f) * Time.deltaTime;
-                //objects[i].GetComponent<SpriteRenderer>().color = new Vector4(0f, 0f, 0f, 0.6f);
-            }
-        }
-
-        if (timer > waitTime && t && durationTimer < duration3)
-        {
-            for (int i = 0; i <= objects.Count - 1; i++)
-            {
-                timer = timer - waitTime;
-                objects[i].transform.position = objects[i].transform.position + new Vector3(1f, 0.0f, 0.0f) * Time.deltaTime;
-                //objects[i].GetComponent<SpriteRenderer>().color = new Vector4(0f, 0f, 0f, 0.8f);
-            }
-        }
-
-        if (timer > waitTime && t && durationTimer < duration4)
-        {
-            for (int i = 0; i <= objects.Count - 1; i++)
-            {
-                timer = timer - waitTime;
-                objects[i].transform.position = objects[i].transform.position + new Vector3(0.5f, 0.0f, 0.0f) * Time.deltaTime;
-                //objects[i].GetComponent<SpriteRenderer>().color = new Vector4(0f, 0f, 0f, 1f);
-            }
-        }
-
-        if (durationTimer > duration4){
-            if (Input.GetKeyDown("t"))
+        if (durationTimer > duration){
+            if (Input.GetKeyDown("t") && t)
             {
                 for (int i = 0; i <= objects.Count - 1; i++)
                 {
+                    objects[i].transform.DOMove(new Vector3(-1, 2, 0), 1).SetEase(Ease.OutQuart);
                     objects[i].SetActive(false);
+                    durationTimer = 0.0f;
                 }
                 t = false;
             }
