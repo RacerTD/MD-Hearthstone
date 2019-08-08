fabcb1e4e5650a43890616280b198c679dd13c05
Niiiiiiiiiiii <nici@bambusource.de>
2019-08-08 07:05:34 +0200

    tmpro test script

---
+++ Assets/Scripts/TextMesh Pro Test/ScoreUpdate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreUpdate : MonoBehaviour
+{
+
+    private TextMeshProUGUI textMesh;
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textMesh = gameObject.GetComponent<TextMeshProUGUI>();
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textMesh.text = score.ToString();
+        score++;
+    }
+}
