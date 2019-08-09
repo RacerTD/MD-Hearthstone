c55d14e1994570448213e1dcc66eaf259e473dab
Jennifer <jebethke@stud.mediadesign.de>
2019-08-09 18:05:58 +0200

    Finished Main Menu, beginning Arrow and wave finished

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    Vector2 startPos;
+    Vector2 endPos;
+    Camera camera;
+    Vector3 camOffset = new Vector3(0, 0, 5);
+    [SerializeField] AnimationCurve ac;
+    void Start()
+    {
+        camera = Camera.main;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (lineRenderer  == null)
+            {
+                lineRenderer = gameObject.AddComponent<LineRenderer>();
+            }
+            lineRenderer.positionCount = 2;
+            startPos = camera.ScreenToWorldPoint(Input.mousePosition) + camOffset;
+            lineRenderer.SetPosition(0, startPos);
+            lineRenderer.useWorldSpace = true;
+            if (Input.GetMouseButton(0))
+            {
+                endPos = camera.ScreenToWorldPoint(Input.mousePosition);
+                lineRenderer.SetPosition(1, endPos);
+            }
+        }
+        
+    }
+    
+}
---
+++ Assets/Scripts/Card Scripts/waveVisibility.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class waveVisibility : MonoBehaviour
+{
+    public List<Sprite> animation = new List<Sprite>();
+    int counter = 0;
+    public Image toAnimate;
+    float time;
+    [Range(2, 0)]
+    public float speed = 1;
+    void Start()
+    {
+        counter = Random.Range(0, animation.Count);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if (time >= speed / 10)
+        {
+            toAnimate.sprite = animation[counter];
+            counter++;
+            time = 0;
+            if (counter >= animation.Count - 1)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
