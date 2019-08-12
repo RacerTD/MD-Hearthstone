925ab1f591fa73ca0f8e56e421b76d9c2e9a8dc1
Alex <alex.albrecht@hotmail.de>
2019-08-12 09:34:09 +0200

    BATest3 creaed; Rotation Script (ongoing); Prefab Test -> Hand Card Prefab

---
+++ Assets/Scripts/BATest Scripts/CardRotation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// [Attribut]: Code will also be called in Editor Mode
+[ExecuteInEditMode]
+public class CardRotation : MonoBehaviour
+{
+    public RectTransform CardFront;
+
+    public RectTransform CardBack;
+
+    // an empty Game Object that is placed a bit above the face of the card, in the center of the card
+    public Transform targetFacePoint;
+
+    public Collider collision;
+
+    // if this is true, player see the card Back
+    private bool showingBack = false;
+
+    // called once per frame
+    private void Update()
+    {
+        // Raycast from Camera to a target point on the face of the card
+        // If it passes through the card's collider, we should show the back of the card
+
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(origin: Camera.main.transform.position,
+            direction: (-Camera.main.transform.position + targetFacePoint.position).normalized,
+            maxDistance: (-Camera.main.transform.position + targetFacePoint.position).magnitude);
+
+        bool passedThroughColliderOnCard = false;
+
+
+
+    }
+}
