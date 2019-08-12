a9379dcc5c0f1a56f49c99c0193074379ca7b65e
Alex <alex.albrecht@hotmail.de>
2019-08-12 11:51:46 +0200

    Card Rotation Script done and ready for implementation

--- Assets/Scripts/BATest Scripts/CardRotation.cs
+++ Assets/Scripts/BATest Scripts/CardRotation.cs
@@ -20,17 +20,44 @@ public class CardRotation : MonoBehaviour
     private bool showingBack = false;
 
     // called once per frame
-    private void Update()
+    void Update()
     {
         // Raycast from Camera to a target point on the face of the card
         // If it passes through the card's collider, we should show the back of the card
 
         RaycastHit[] hits;
         hits = Physics.RaycastAll(origin: Camera.main.transform.position,
+            // direction: target - Camera. / .normalized means magnitude is equal to 1 
             direction: (-Camera.main.transform.position + targetFacePoint.position).normalized,
-            maxDistance: (-Camera.main.transform.position + targetFacePoint.position).magnitude);
+            maxDistance: (-Camera.main.transform.position + targetFacePoint.position).magnitude) ;
+
 
         bool passedThroughColliderOnCard = false;
+       
+        // sets bool passedThruoghColliderOnCard to true if collider gets hit
+        foreach (RaycastHit hitsHit in hits)
+        {
+            if (hitsHit.collider == collision)
+                passedThroughColliderOnCard = true;
+        }
+
+        if (passedThroughColliderOnCard != showingBack)
+        {
+            showingBack = passedThroughColliderOnCard;
+            if (showingBack)
+            {
+                CardFront.gameObject.SetActive(false);
+                CardBack.gameObject.SetActive(true);
+            }
+            else
+            {
+                CardFront.gameObject.SetActive(true);
+                CardBack.gameObject.SetActive(false);
+            }
+
+        }
+
+
 
 
 
