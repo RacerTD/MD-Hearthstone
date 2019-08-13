33550b612fa7de3ce7bdc2820ac0156a8836a573
Niiiiiiiiiiii <nici@bambusource.de>
2019-08-13 14:42:18 +0200

    Flattening 9, neue karten (wip)

--- Assets/Scripts/Card Scripts/NameTooltip.cs
+++ Assets/Scripts/Card Scripts/NameTooltip.cs
@@ -5,7 +5,8 @@ using DG.Tweening;
 
 public class NameTooltip : MonoBehaviour
 {
-    public GameObject box;
+    public List<GameObject> objects = new List<GameObject>();
+    //public GameObject box;
     bool visible = false;
 
     // Start is called before the first frame update
@@ -36,21 +37,38 @@ public class NameTooltip : MonoBehaviour
 
     private void moveNameTip()
     {
-        box.transform.DOScale(1f, 0f).SetEase(Ease.OutQuart);
+
+        for (int i = 0; i <= objects.Count - 1; i++)
+        {
+            objects[i].transform.position = objects[i].transform.position + new Vector3(-20f, 0, 0);
+            objects[i].transform.DOScale(1f, 0f).SetEase(Ease.OutQuart);
+            objects[i].SetActive(true);
+            objects[i].transform.DOLocalMove(new Vector3(685, 350, 0), 0.7f).SetEase(Ease.OutQuart);
+        }
+        /*box.transform.DOScale(1f, 0f).SetEase(Ease.OutQuart);
         box.transform.position = box.transform.position + new Vector3(0, -20f, 0);
         box.SetActive(true);
-        box.transform.DOLocalMove(new Vector3(447, 600, 0), 0.7f).SetEase(Ease.OutQuart);
+        box.transform.DOLocalMove(new Vector3(447, 600, 0), 0.7f).SetEase(Ease.OutQuart);*/
     }
 
     private void HideTip()
     {
-        box.transform.DOScale(0.7f, 0.2f).SetEase(Ease.InQuart);
+        for (int i = 0; i <= objects.Count - 1; i++)
+        {
+            objects[i].transform.DOScale(0.7f, 0.2f).SetEase(Ease.InQuart);
+            //StartCoroutine(Hide());
+        }
+        //box.transform.DOScale(0.7f, 0.2f).SetEase(Ease.InQuart);
         StartCoroutine(Hide());
     }
 
     IEnumerator Hide()
     {
         yield return new WaitForSecondsRealtime(0.2f);
-        box.SetActive(false);
+        for (int i = 0; i <= objects.Count - 1; i++)
+        {
+            objects[i].SetActive(false);
+        }
+        //box.SetActive(false);
     }
 }
