6b637006204e6e631c95f63ad4a7b7290fdfcb77
Alex <alex.albrecht@hotmail.de>
2019-08-13 09:29:46 +0200

    BATest3 changes; SOScripts implemented; Hover Script Test started

---
+++ Assets/Scripts/BATest Scripts/SO Asset Scripts/Card Asset/CardUnityIntegration.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+
+/// <summary>
+/// Specifies the path to Asset (as Attribute) and access to 
+/// ScriptableObjectUtility to create an Asset with the name <CardAsset>
+/// </summary>
+
+static class CardUnityIntegration
+{
+    [MenuItem("Assets/Create/CardAsset")]
+    public static void CreateYourScriptableObject()
+    {
+        ScriptableObjectUtility.CreateAsset<CardAsset>();
+    }
+}
---
+++ Assets/Scripts/BATest Scripts/SO Asset Scripts/ScriptableObjectUtility.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public static class ScriptableObjectUtility
+{
+    ///Create new asset from SO type with unique name at
+    ///selected folder in project window.
+    public static void CreateAsset<T>() where T : ScriptableObject
+    {
+        var asset = ScriptableObject.CreateInstance<T>();
+        ProjectWindowUtil.CreateAsset(asset, "New " + typeof(T).Name + " .asset");
+    }
+}
