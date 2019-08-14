dcc8cc5fd8ee5576c3373c667e682b37e14945b0
Niiiiiiiiiiii <nici@bambusource.de>
2019-08-14 16:30:30 +0200

    particles

---
+++ Assets/Scripts/Particles/Damage.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Damage : MonoBehaviour
+{
+    public GameObject damageParticle;
+    public float spawnRate = 0.2f;
+    public float nextSpawn = 1.1f;
+    public float speed;
+    bool enable = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        StartCoroutine(KillSystem());
+        if (Time.time > nextSpawn && enable)
+        {
+            nextSpawn = Time.time + (spawnRate + 0.2f);
+            Vector2 newPos = transform.position + new Vector3(UnityEngine.Random.Range(-50, 50), UnityEngine.Random.Range(-50, 50), UnityEngine.Random.Range(-50, 50));
+            GameObject myObject = Instantiate(damageParticle, newPos, Quaternion.identity);
+            myObject.transform.SetParent(transform);
+        }
+    }
+
+    IEnumerator KillSystem()
+    {
+        yield return new WaitForSecondsRealtime(2f);
+        enable = false;
+        yield return new WaitForSecondsRealtime(2f);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Particles/DamageMove.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class DamageMove : MonoBehaviour
+{
+    private Damage damage;
+
+    public float time_min = 1f;
+    public float time_max = 3f;
+    public Sprite[] sprites = new Sprite[12];
+    public Image image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ShowRandomImage());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + new Vector3(0, 1, 0);
+        StartCoroutine(Fade());
+        StartCoroutine(Kill());
+    }
+    IEnumerator Kill()
+    {
+        yield return new WaitForSecondsRealtime(2f);
+        Destroy(gameObject);
+    }
+    IEnumerator Fade()
+    {
+        // fade from opaque to transparent
+        // loop over 1 second backwards
+        yield return new WaitForSecondsRealtime(1f);
+        for (float i = 1; i >= 0; i -= Time.deltaTime)
+        {
+            // set color with i as alpha
+            image.color = new Color(1, 1, 1, i);
+            yield return null;
+        }
+    }
+    public IEnumerator ShowRandomImage()
+    {
+        while (true)
+        {
+            image.sprite = sprites[Random.Range(0, sprites.Length)];
+            image.enabled = true;
+            yield return new WaitForSeconds(1);
+            image.enabled = false;
+            yield return new WaitForSeconds(Random.Range(time_min, time_max));
+        }
+
+
+    }
+}
