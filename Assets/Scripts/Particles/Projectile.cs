5f261116865ce6c9b304b9ba39884fd29f0af9d5
Niiiiiiiiiiii <nici@bambusource.de>
2019-08-19 16:36:52 +0200

    Rico.asset

---
+++ Assets/Scripts/Particles/Projectile.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public GameObject projectile;
+    //public float spawnRate = 0.2f;
+    public float nextSpawn = 1.1f;
+    public float speed;
+    bool enable = true;
+    public Vector3 offset; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(KillSystem());
+        if (Time.time > nextSpawn && enable)
+        {
+            Vector2 newPos = transform.position + offset;
+            GameObject myObject = Instantiate(projectile, newPos, Quaternion.identity);
+            myObject.transform.SetParent(transform);
+        }
+    }
+
+    IEnumerator KillSystem()
+    {
+        yield return new WaitForSecondsRealtime(0.1f);
+        enable = false;
+        yield return new WaitForSecondsRealtime(1f);
+        Destroy(gameObject);
+        Destroy(transform.parent.gameObject);
+    }
+}
---
+++ Assets/Scripts/Particles/ProjectileMove.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class ProjectileMove : MonoBehaviour
+{
+    private Projectile projectile;
+    public Image img;
+    public Vector3 targetLocation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + new Vector3(0, 1, 0);
+        StartCoroutine(Fade());
+        StartCoroutine(Kill());
+    }
+    IEnumerator Kill()
+    {
+        transform.DOMove(targetLocation, 1);
+        yield return new WaitForSecondsRealtime(2f);
+        Destroy(gameObject);
+    }
+    IEnumerator Fade()
+    {
+        // fade from opaque to transparent
+        // loop over 1 second backwards
+        yield return new WaitForSecondsRealtime(1f);
+        for (float i = 1; i >= 0; i -= Time.deltaTime)
+        {
+            // set color with i as alpha
+            img.color = new Color(1, 1, 1, i);
+            yield return null;
+        }
+    }
+}
