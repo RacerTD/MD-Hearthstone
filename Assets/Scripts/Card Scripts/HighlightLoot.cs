96c6a458ef114456c0d5dc4ff81794965626cd12
Jennifer <jebethke@stud.mediadesign.de>
2019-08-21 11:48:23 +0200

    working Loot Highlight

---
+++ Assets/Scripts/Card Scripts/HighlightLoot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighlightLoot : MonoBehaviour
+{
+    public Color highlightWavesColor;
+    public Color defaultWavesColor;
+    public GameObject LootWaves;
+    public GameManager gameManager;
+    
+    void Awake()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+    void Update()
+    {
+        if (gameManager.lootEnabler != null)
+        {
+                GetComponent<Image>().color = highlightWavesColor;
+          
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            GetComponent<Image>().color = defaultWavesColor;
+
+            return;
+        }
+    }
+}
